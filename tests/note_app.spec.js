a90ffcc86f2cd96dd132373c36d5e7ebd3b68a15
Frederico <fozg@gft.com>
2025-03-10 12:29:52 +0100

    5.18 step 2

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,8 +1,17 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
-// const { test, after, beforeEach, describe } = require('node:test')
+const axios = require('axios');
 
 describe('Blog app', () => {
   beforeEach(async ({ page }) => {
+    await axios.delete('http://localhost:3003/api/users')
+    await axios.delete('http://localhost:3003/api/blogs')
+
+    await axios.post('http://localhost:3003/api/users', {
+      name: 'test name',
+      username: 'testUser',
+      password: 'testPass'
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -14,5 +23,24 @@ describe('Blog app', () => {
 
     const locatorPass = await page.getByText('password')
     await expect(locatorPass).toBeVisible()
+  })
+
+  
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').first().fill('testUser')
+      await page.getByTestId('password').fill('testPass')
+      await page.getByRole('button', { name: 'login' }).click()
+      
+      await expect(page.getByText('test name logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').first().fill('testUserWrong')
+      await page.getByTestId('password').fill('testPass')
+      await page.getByRole('button', { name: 'login' }).click()
+      
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
   })
 })
