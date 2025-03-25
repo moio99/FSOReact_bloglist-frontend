5a5bbd89c35aafd90efd45213abea04b479f9f19
Frederico <fozg@gft.com>
2025-03-25 17:52:06 +0100

    8.10

--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -1,4 +1,16 @@
 import { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+const ADD_BOOK = gql`
+  mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,25 +19,57 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [addBook, { loading, error }] = useMutation(ADD_BOOK, {
+    onCompleted: () => {
+      setTitle('')
+      setAuthor('')
+      setPublished('')
+      setGenres([])
+      setGenre('')
+    }
+  })
+
   if (!props.show) {
     return null
   }
 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    if (!title || !author || !published || genres.length === 0) {
+      alert("All fields are required")
+      return
+    }
 
     setTitle('')
     setPublished('')
     setAuthor('')
     setGenres([])
     setGenre('')
+
+    try {
+      const bookAdded = await addBook({
+        variables: {
+          title,
+          author,
+          published: parseInt(published),
+          genres
+        }
+      })
+      console.log('bookAdded', bookAdded)
+    } catch (err) {
+      console.error("Error al añadir el libro:", err.message)
+    }
   }
 
   const addGenre = () => {
-    setGenres(genres.concat(genre))
-    setGenre('')
+    if (genre.trim() && !genres.includes(genre)) {
+      setGenres([...genres, genre])
+      setGenre('')
+    }
   }
 
   return (
